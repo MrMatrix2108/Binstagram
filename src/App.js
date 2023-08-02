aac01a76f7188d6b4702ebd66c77f4f2504b6398
Jishen Harilal <ST10083511@vcconnect.edu.za>
2023-08-02 14:17:18 +0200

    SSL Certificate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+// App.js
+
+import React from 'react';
 import './App.css';
+import Feed from './Feed';
+import Banner from './Banner';
+import Navigation from './Navigation';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+    <Banner />
+    <Navigation/>
+      <Feed />
     </div>
   );
 }
---
+++ src/Navigation.js
@@ -0,0 +1,37 @@
+// Navigation.js
+
+import React from 'react';
+import './Navigation.css';
+
+const Navigation = () => {
+  return (
+    <div>
+        
+    <nav className="navigation">
+    <div>
+        <h1 className="logo">Binstagram</h1>
+        </div>
+      <ul>
+        <li>
+          <i className="fas fa-home"></i>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <i className="fas fa-user-friends"></i>
+          <a href="#friends">Friends</a>
+        </li>
+        <li>
+          <i className="fas fa-info-circle"></i>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <i className="fas fa-envelope"></i>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+    </div>
+  );
+};
+
+export default Navigation;
