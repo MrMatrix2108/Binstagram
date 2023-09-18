69985f4d92567a8a76b510e5b3349c553dabac21
Jishen Harilal <ST10083511@vcconnect.edu.za>
2023-09-18 12:02:48 +0200

    Likes update and Posts load from api

--- src/Post.js
+++ src/Post.js
@@ -3,35 +3,42 @@
 import React, { useState } from 'react';
 import './Post.css';
 
-const Post = ({ username, profileImg ,imageSrc, likes, comments }) => {
+const Post = ({postId, imageSrc, caption, likes}) => {
+
   const [likeCount, setLikeCount] = useState(likes);
-  const [commentCount, setCommentCount] = useState(comments);
 
-  const handleLike = () => {
-    setLikeCount(likeCount + 1);
-  };
+  const processLike = async () => {
+    try {
+      const response = await fetch('api/posts/like', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ _id: postId }),
+      });
 
-  const handleComment = () => {
-    setCommentCount(commentCount + 1);
+      if (response.ok) {
+        const updatedPost = await response.json();
+        setLikeCount(updatedPost.likes);
+      } else {
+        console.error('Error:', response.statusText);
+        console.log('ID', postId);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
   };
 
   return (
-    <div className="post">  
-    <div className="profile-info">
-        <div className="profile-pic">
-        <img src={profileImg} alt="Profile" />
-        </div>
-        <div className="username">{username}</div>
+    <div className="post">
+      <img src={imageSrc} alt="Post"/>
+      <div 
+      className="caption">{caption}
       </div>
-      <img src={imageSrc} alt="Post" />
       <div className="actions">
-        <button onClick={handleLike}>
+      <button onClick={processLike}>
           <i className="far fa-heart"></i> {/* Heart icon */}
           <span>{likeCount}</span>
-        </button>
-        <button onClick = {handleComment}>
-          <i className="far fa-comment"></i> {/* Comment icon */}
-          <span>{commentCount}</span>
         </button>
       </div>
     </div>
@@ -39,3 +46,25 @@ const Post = ({ username, profileImg ,imageSrc, likes, comments }) => {
 };
 
 export default Post;
+
+
+// <div className="post">  
+  //   <div className="profile-info">
+  //       <div className="profile-pic">
+  //       <img src={profileImg} alt="Profile" />
+  //       </div>
+  //       <div className="username">{username}</div>
+  //     </div>
+  //     <img src={imageSrc} alt="Post" />
+  //     <div className="actions">
+  //       <button onClick={handleLike}>
+  //         <i className="far fa-heart"></i> {/* Heart icon */}
+  //         <span>{likeCount}</span>
+  //       </button>
+  //       <button onClick = {handleComment}>
+  //         <i className="far fa-comment"></i> {/* Comment icon */}
+  //         <span>{commentCount}</span>
+  //       </button>
+  //     </div>
+  //   </div>
+
